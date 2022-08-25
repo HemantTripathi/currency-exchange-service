fbed3d023aa81524d1de659044d27b64db211f62
HemantTripathi <hemant.tripathi20@gmail.com>
2022-08-25 10:56:19 +0530

    Initial commit - currency exchage service

---
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,28 @@
+package com.in28minutes.microservices.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController // Since we want to expose the RestAPI from this controller class
+public class CurrencyExchangeController {
+
+    //step 1: our aim is to create the below URL
+    //http://localhost:8000/currency-exchange/from/USD/to/INR
+    // instead of USD and INR we would make it available at runtime
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
+        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
+    }
+}
