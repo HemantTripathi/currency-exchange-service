8bad5cb301ef7aebea742ac4373d09fb6a8b1673
HemantTripathi <hemant.tripathi20@gmail.com>
2022-09-06 19:33:23 +0530

    Added feature to fetch data from h2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,22 @@
+INSERT
+INTO
+  currency_exchange
+  (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES
+  (10001, 'USD', 'INR', 75, '');
+
+INSERT
+INTO
+  currency_exchange
+  (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES
+  (10002, 'EUR', 'INR', 90, '');
+
+INSERT
+INTO
+  currency_exchange
+  (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES
+  (10003, 'AUD', 'INR', 50, '');
+
+
